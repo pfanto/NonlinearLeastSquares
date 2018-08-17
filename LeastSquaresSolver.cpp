fdeb3dcde1d2cc73f35dec8cc6201ec7f4e21147
Paul Fanto <paul.fanto@yale.edu>
2018-08-16 18:53:46 -0600

    got sensitivity matrix

--- LeastSquaresSolver.cpp
+++ LeastSquaresSolver.cpp
@@ -7,4 +7,18 @@ void LeastSquaresSolver(const int n, const int m, const int npars,
 	cout << setprecision(8);
 
 	cout << "Least Squares Solver" << endl;
+
+	double ** S = new double * [n]; // sensitivity matrix
+	cout << "S = " << endl;
+	for (int i = 0; i < n; i++) {
+		S[i] = new double [m];
+		double xi = x[i];
+		for (int k = 0; k < m; k++) {
+			S[i][k] = Sensitivity(xi,k,npars,a,(*model));
+		}
+		PrintPointerArray("",S[i],m);
+	}
+
+	// memory deallocation
+	for (int i = 0; i < n; i++) delete [] S;
 }
--- Sensitivity.hpp
+++ Sensitivity.hpp
@@ -9,6 +9,6 @@ using namespace std;
 
 #include "writeout.hpp"
 
-double Sensitivity(const double xi, const int k, const int npars, double * a,double (*model) (const double, const int, double * ));
+double Sensitivity(const double xi, const int k, const int npars, double * a, double (*model) (const double, const int, double * ));
 
 #endif
