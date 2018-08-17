983ce8d9414c049bd0c10711eeb6a5abda8acf25
Paul Fanto <paul.fanto@yale.edu>
2018-08-16 23:53:22 -0600

    got an easy convergence for good guess with LM

--- LeastSquaresSolver.cpp
+++ LeastSquaresSolver.cpp
@@ -36,7 +36,10 @@ void LeastSquaresSolver(const int n, const int m, const int npars,
 	}
 	cout << "loss = " << loss << endl;
 
+	double lambda = lambda_initial*1.0;
+
 	for (int it = 0; it < NSTEP; it++) {
+		cout << "lambda = " << lambda << endl;
 		// set up H and b
 		for (int k = 0; k < m; k++) {
 			b[k] = 0.0;
@@ -44,15 +47,17 @@ void LeastSquaresSolver(const int n, const int m, const int npars,
 				b[k] += S[i][k]*(y[i]-f[i]);
 			}
 
-			for (int l = 0; l < m; l++) {
+			for (int l = k; l < m; l++) {
 				H[k][l] = 0.0;
 				for (int i = 0; i < n; i++) {
 					H[k][l] += S[i][k]*S[i][l];
 				}
+				H[l][k] = H[k][l]*1.0;
 			}
+			H[k][k] = H[k][k] + lambda*H[k][k];
 		}
 		//cout << "b = " << b[0] << endl;
-		//cout << "H = " << H[0][0] << endl;
+		cout << "H = " << H[0][0] << endl;
 
 		// solve for da
 		symsolve(m,da,H,b);
@@ -75,18 +80,35 @@ void LeastSquaresSolver(const int n, const int m, const int npars,
 		// calculate new loss
 		// update S and f as well
 		double loss_new = 0.0;
-		cout << "loss_new = " << loss_new << endl;
+
 		for (int i = 0; i < n; i++) {
 			f[i] = (*model)(x[i],npars,a);
 			loss_new += (y[i]-f[i])*(y[i]-f[i]);
 			for (int k = 0; k < m; k++) S[i][k] = Sensitivity(x[i],k,npars,a,(*model));
 		}
+		cout << "loss_new = " << loss_new << endl;
 
 		// convergence criterion
+
 		if(abs(loss_new-loss) <= STATIONARY_TOL) {
 			convergence = true;
 			break;
 		}
+		
+		if (loss_new >= loss) {
+			lambda *= 10.0;
+			for (int k = 0; k < m; k++) a[k] -= da[k];
+			// re-update f[i] and S
+			for (int i = 0; i < n; i++) {
+				f[i] = (*model)(x[i],npars,a);
+				for (int k = 0; k < m; k++) S[i][k] = Sensitivity(x[i],k,npars,a,(*model));
+			}
+
+		}
+		else {
+			lambda /= 10.0;
+		}
+		
 
 		// update loss
 		loss = loss_new*1.0;
