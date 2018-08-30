6490116b17626e07c52d69de0cf385b4b0a00ca4
Paul Fanto <paul.fanto@yale.edu>
2018-08-30 16:27:53 -0600

    Update LeastSquaresSolver.hpp

--- source/LeastSquaresSolver.hpp
+++ source/LeastSquaresSolver.hpp
@@ -11,10 +11,12 @@ using namespace std;
 #include "writeout.hpp"
 #include "lapackwrapper.hpp"
 
+// Levenberg-Marquardt minimization
 void LeastSquaresSolver(const int n, const int m, const int npars, 
 	double * x, double * y, double * a, double (*model)(const double, const int, double *),
 	const int NSTEP, const double STATIONARY_TOL, const double lambda_initial);
 
+// nonlinear least-squares minimization using inverse Hessian with regularization
 void LeastSquaresSolverRegularized (const int n, const int m, const int npars, 
 	double * x, double * y, double * a, double (*model)(const double, const int, double *),
 	const int NSTEP, const double STATIONARY_TOL, const double mu);
